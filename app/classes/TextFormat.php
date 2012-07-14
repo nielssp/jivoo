d67226577f03ef380586903bfb94c3955bc6a547
Niels Poulsen <nielssonnich@gmail.com>
2012-07-14 21:18:32 +0200

    Experimenting with TextFormat... Not done yet though.

--- app/classes/TextFormat.php
+++ app/classes/TextFormat.php
@@ -2,14 +2,16 @@
 class TextFormat implements IContentFormat {
   
   public function toHtml($text) {
-    $html = str_replace("\n", "<br />\n", $text);
+    $html = str_replace("\n\n", "</p><p>", $text);
+    $html = str_replace("\n", "<br />\n", $html);
     /** @todo Improve URL-detection */
     $html = preg_replace('/ (https?:\/\/[\S]+) /i', ' <a href="\\1">\\1</a> ', $html);
-    return $html;
+    return '<p>' . $html . '</p>';
   }
   
   public function fromHtml($html) {
     $text = str_replace("\n", '', $html);
+    $text = preg_replace('/<\/p><p>/i', "\n\n", $text);
     $text = preg_replace('/<br +\/?>/i', "\n", $text);
     return $text;
   }
