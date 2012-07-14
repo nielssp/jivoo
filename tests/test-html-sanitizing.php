5f53a4615fb09595302ddad56b6919ff3a1ad451
Niels Poulsen <nielssonnich@gmail.com>
2012-07-14 14:15:47 +0200

    Working HTML stripping...

--- tests/test-html-sanitizing.php
+++ tests/test-html-sanitizing.php
@@ -3,23 +3,83 @@
 class Format {
   private $allow = array();
 
-  function replaceTags($matches) {
-    var_dump($matches);
+  private $openTags = array();
+
+  private function replaceAttributes($tag, $attributes) {
+    preg_match_all('/\s+(\w+)(\s*=\s*((?:".*?"|\'.*?\'|[^\'">\s]+)))?/', $attributes, $matches);
+    $attributes = $matches[1];
+    $values = $matches[3];
+    $num = count($attributes);
+    $newString = '';
+    foreach ($attributes as $key => $attribute) {
+      if (isset($this->allow[$tag][$attribute])) {
+        $value = $values[$key];
+        if (isset($this->allow[$tag][$attribute]['url'])) {
+          if (preg_match('/^("|\')?https?:\/\//i', $value) == 0) {
+            return FALSE;
+          }
+        }
+        $newString .= ' ' . $attribute . '=' . $value;
+      }
+    }
+    return $newString;
+  }
+
+  private function openTag($tag) {
+    if (!isset($this->openTags[$tag])) {
+      $this->openTags[$tag] = 0;
+    }
+    $this->openTags[$tag]++;
+  }
+
+  private function closeTag($tag) {
+    if (!isset($this->openTags[$tag])) {
+      $this->openTags[$tag] = 0;
+    }
+    $this->openTags[$tag]--;
+  }
+
+  private function replaceTag($matches) {
     $num = count($matches);
     $tag = $matches[2];
+    $attributes = $matches[3];
     $isCloseTag = $matches[1] == '/';
     $selfClosing = $matches[$num - 1] == '/';
+    if ($tag == 'br') {
+      $selfClosing = TRUE;
+    }
     if (isset($this->allow[$tag])) {
-      return '<' . $tag . ($selfClosing ? '/' : '') . '>';
+      if ($isCloseTag AND (!isset($this->openTags[$tag])
+          OR $this->openTags[$tag] < 1)) {
+        return '';
+      }
+      $attributes = $this->replaceAttributes($tag, $attributes);
+      if ($attributes !== FALSE ) {
+        $clean = '<' . ($isCloseTag ? '/' : '');
+        $clean .= $tag;
+        $clean .= $attributes;
+        $clean .= ($selfClosing ? ' /' : '') . '>';
+        if ($isCloseTag) {
+          $this->closeTag($tag);
+        }
+        else if (!$selfClosing) {
+          $this->openTag($tag);
+        }
+        return $clean;
+      }
     }
     return '';
   }
 
-  function strip($text, $allow = array()) {
+  public function strip($text, $allow = array()) {
     $this->allow = $allow;
     $text =
-      preg_replace_callback('/<(\/?)(\w+)((\s+\w+(\s*=\s*(?:".*?"|\'.*?\'|[^\'">\s]+))?)+\s*|\s*)(\/?)>/', array($this, 'replaceTags'), $text);
-    print_r($matches);
+      preg_replace_callback('/<(\/?)(\w+)((\s+\w+(\s*=\s*(?:".*?"|\'.*?\'|[^\'">\s]+))?)+\s*|\s*)(\/?)>/', array($this, 'replaceTag'), $text);
+    foreach ($this->openTags as $tag => $number) {
+      for ($i = 0; $i < $number; $i++) {
+        $text .= '</' . $tag . '>';
+      }
+    }
     return $text;
   }
 }
@@ -34,9 +94,10 @@ $comment = <<<END
 <a href="javascript:alert('xss');" onmouseover="alert('xss');">Naked ladies</a><br/>
   <br>
   <p>Hello, World</p>
-</div>
+  </div><div>
   <div style=color:blue>this is green</div>
   <meta name="derp" value="lort"/>
+  <a href="http://apakoh.dk">Hello</a>
 END;
 
 output($comment);
@@ -46,7 +107,9 @@ output(nl2br(strip_tags($comment)));
 $allow = array(
   'div' => array(),
   'br' => array(),
-  'a' => array('href'),
+  'a' => array(
+    'href' => array('url' => TRUE)
+  ),
   'p' => array()
 );
 
