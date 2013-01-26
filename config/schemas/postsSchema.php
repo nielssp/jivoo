df3ba225385237412e57ec2c50bf17fcb843aa59
Niels Poulsen <nielssonnich@gmail.com>
2013-01-26 10:56:27 +0100

    Fixed some eclipse format fuckup

--- config/schemas/postsSchema.php
+++ config/schemas/postsSchema.php
@@ -5,37 +5,81 @@
  * @subpackage Schemas
  */
 class postsSchema extends Schema {
-  public $id = array('type' => 'integer', 'length' => 10, 'key' => 'primary',
-    'autoIncrement' => true, 'null' => false,
-  );
-
-  public $name = array('type' => 'string', 'length' => 255, 'key' => 'unique',
+  public $id = array(
+    'type' => 'integer',
+    'length' => 10,
+    'key' => 'primary',
+    'autoIncrement' => true,
     'null' => false,
   );
 
-  public $title = array('type' => 'string', 'length' => 255, 'null' => false,);
-
-  public $content = array('type' => 'text', 'null' => false,);
-
-  public $date = array('type' => 'integer', 'length' => 10, 'key' => 'index',
+  public $name = array(
+    'type' => 'string',
+    'length' => 255,
+    'key' => 'unique',
     'null' => false,
   );
 
-  public $comments = array('type' => 'integer', 'length' => 10, 'null' => false,);
-
-  public $state = array('type' => 'string', 'length' => 50, 'null' => false,);
-
-  public $commenting = array('type' => 'string', 'length' => 10,
+  public $title = array(
+    'type' => 'string',
+    'length' => 255,
     'null' => false,
   );
 
-  public $user_id = array('type' => 'integer', 'length' => 10, 'null' => false,);
+  public $content = array(
+    'type' => 'text',
+    'null' => false,
+  );
 
-  public $status = array('type' => 'string', 'length' => 50, 'null' => false,);
+  public $date = array(
+    'type' => 'integer',
+    'length' => 10,
+    'key' => 'index',
+    'null' => false,
+  );
+
+  public $comments = array(
+    'type' => 'integer',
+    'length' => 10,
+    'null' => false,
+  );
+
+  public $state = array(
+    'type' => 'string',
+    'length' => 50,
+    'null' => false,
+  );
+
+  public $commenting = array(
+    'type' => 'string',
+    'length' => 10,
+    'null' => false,
+  );
+
+  public $user_id = array(
+    'type' => 'integer',
+    'length' => 10,
+    'null' => false,
+  );
+
+  public $status = array(
+    'type' => 'string',
+    'length' => 50,
+    'null' => false,
+  );
 
   public $indexes = array(
-    'PRIMARY' => array('columns' => array('id'), 'unique' => true),
-    'name' => array('columns' => array('name'), 'unique' => true),
-    'date' => array('columns' => array('date'), 'unique' => false),
+    'PRIMARY' => array(
+      'columns' => array('id'),
+      'unique' => true
+    ),
+    'name' => array(
+      'columns' => array('name'),
+      'unique' => true
+    ),
+    'date' => array(
+      'columns' => array('date'),
+      'unique' => false
+    ),
   );
 }
