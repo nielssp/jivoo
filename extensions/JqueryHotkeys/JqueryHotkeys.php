e414d147c07b31398b82465845f3749589dc2855
Niels Poulsen <nielssonnich@gmail.com>
2013-01-28 15:08:58 +0100

    Fix missing Assets-dependency in JqueryHotkeys

--- extensions/JqueryHotkeys/JqueryHotkeys.php
+++ extensions/JqueryHotkeys/JqueryHotkeys.php
@@ -4,14 +4,14 @@
 // Category     : JavaScript jQuery
 // Website      : http://code.google.com/p/js-hotkeys/
 // Version      : 0.7.9
-// Dependencies : Templates ext;Jquery
+// Dependencies : Templates Assets ext;Jquery
 
 class JqueryHotkeys extends ExtensionBase {
   protected function init() {
-    $this->m
-      ->Templates
-      ->addScript('jquery-hotkeys',
-        //      $this->getLink('js/jquery.hotkeys-0.7.9.min.js'),
-        $this->getAsset('js/jquery.hotkeys-0.7.9-fix.js'), array('jquery'));
+    $this->m->Templates->addScript(
+      'jquery-hotkeys',
+      $this->getAsset('js/jquery.hotkeys-0.7.9-fix.js'),
+      array('jquery')
+    );
   }
 }
