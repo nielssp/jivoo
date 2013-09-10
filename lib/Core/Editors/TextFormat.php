12a1aded71b24c2c78630b509b44127532171692
Niels Poulsen <nielssonnich@gmail.com>
2013-09-10 16:14:06 +0200

    Fix url detection in TextFormat

--- lib/Core/Editors/TextFormat.php
+++ lib/Core/Editors/TextFormat.php
@@ -10,8 +10,8 @@ class TextFormat implements IContentFormat {
     $html = preg_replace('/((\r\n|\n\r|\n|\r) *){2}/i', "</p><p>", $text);
     $html = preg_replace('/(\r\n|\n\r|\n|\r)/i', "<br />\n", $html);
     /** @todo Improve URL-detection */
-    $html = preg_replace('/([\n\r \A])(https?:\/\/([^\n\r"< \Z]+))/i',
-      '\\1<a href="\\2">\\3</a>', $html);
+    $html = preg_replace('/(https?:\/\/([^\n\r"< \Z()]+))/i',
+      '<a href="\\1">\\2</a>', $html);
     if ($html == '') {
       return $html;
     }
@@ -24,8 +24,8 @@ class TextFormat implements IContentFormat {
     $text = preg_replace('/<\/p> *<p>/i', "\n\n", $text);
     $text = preg_replace('/<br *\/?>/i', "\n", $text);
     $text = preg_replace(
-      '/([\n\r \A])<a href="(https?:\/\/.+?)">(.*?)<\/a>([\n\r "]|\Z)/i',
-      '\\1\\2\\4', $text);
+      '/<a href="(https?:\/\/.+?)">(.*?)<\/a>([\n\r "()]|\Z)/i',
+      '\\1\\3', $text);
     return $text;
   }
 }
