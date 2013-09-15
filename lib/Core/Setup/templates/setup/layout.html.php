5c471084bb39aadb968c81dfbd5a9edda84a16fa
Niels Poulsen <nielssonnich@gmail.com>
2013-09-15 15:51:41 +0200

    Update setup layout

---
+++ lib/Core/Setup/templates/setup/layout.html.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title><?php echo $title; ?></title>
+
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" type="text/css" href="<?php echo $basicStyle; ?>" />
+
+</head>
+<body>
+
+<div id="header">
+<div class="right"><?php echo $app['name']; ?></div>
+</div>
+
+<div id="content">
+
+<?php echo $this->block('content'); ?>
+
+</div>
+
+<div class="footer" id="poweredby">
+<?php if (isset($app['website'])): ?>
+<?php echo $Html->link(
+  $app['name'] . ' ' . $app['version'],
+  $app['website']
+); ?>
+<?php else: ?>
+<?php echo $app['name']; ?> 
+<?php echo $app['version']; ?>
+<?php endif; ?>
+</div>
+
+<div class="footer" id="links">
+<a href="http://apakoh.dk">Apakoh Core</a>
+</div>
+
+</body>
+</html>
