74532a788399ec6cff267ec01aecbc8571b43b5b
Niels Sonnich Poulsen <nielssonnich@gmail.com>
2014-08-04 15:41:02 +0200

    List applications in ADK

--- app/adk/controllers/AppController.php
+++ app/adk/controllers/AppController.php
@@ -1,6 +1,33 @@
 <?php
 class AppController extends Controller {
   public function index() {
+    $libs = array();
+    $files = scandir(LIB_PATH);
+    if ($files !== false) {
+      foreach ($files as $file) {
+        if ($file[0] == '.') {
+          continue;
+        }
+        if (is_dir(LIB_PATH . '/' . $file)) {
+          $libs[] = $file;
+        }
+      }
+    }
+    $this->libs = $libs;
+    $apps = array();
+    $this->appDir = $this->p('app') . '/..';
+    $files = scandir($this->appDir);
+    if ($files !== false) {
+      foreach ($files as $file) {
+        if ($file[0] == '.') {
+          continue;
+        }
+        if (file_exists($this->appDir . '/' . $file . '/app.php')) {
+          $apps[] = include $this->appDir . '/' . $file . '/app.php';
+        }
+      }
+    }
+    $this->apps = $apps;
     return $this->render();
   }
 }
--- app/adk/templates/index.html.php
+++ app/adk/templates/index.html.php
@@ -1,1 +1,16 @@
-Welcome to Jivoo ADK
+<p>Welcome to Jivoo ADK</p>
+
+<h2>Libraries (<?php echo LIB_PATH; ?>)</h2>
+
+<?php foreach ($libs as $lib): ?>
+<p><?php echo $lib; ?></p>
+<?php endforeach; ?>
+
+<h2>Applications</h2>
+
+<?php foreach ($apps as $app): ?>
+<p>
+<?php echo $app['name'] . ' ' . $app['version']; ?>
+</p>
+<?php endforeach; ?>
+
