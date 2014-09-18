cd6b6c2e58fcd368003d5635dc8d00d206339c3e
Niels Sonnich Poulsen <nielssonnich@gmail.com>
2014-09-18 21:42:09 +0200

    Testing alternative configuration formats

--- tests/conifguration-serialize-test.php
+++ tests/conifguration-serialize-test.php
@@ -39,58 +39,90 @@ function php_pretty_printer($data, $prefix = '') {
 }
 
 function include_config() {
-  return include '../config/test-config.php';
+  return include './config/test-config.php';
+}
+function include_config2() {
+  return include '../app/jivoo/app.php';
 }
 
 function eval_config() {
-  $content = file_get_contents('../config/test-config.php');
+  $content = file_get_contents('./config/test-config.php');
+  $content = str_replace('<?php', '', $content);
+  return eval($content);
+}
+
+function eval_config2() {
+  $content = file_get_contents('../app/jivoo/app.php');
   $content = str_replace('<?php', '', $content);
   return eval($content);
 }
 
 function decode_config() {
-  $content = file_get_contents('../config/test-config.json');
+  $content = file_get_contents('./config/test-config.json');
+  return json_decode($content, true);
+}
+function decode_config2() {
+  $content = file_get_contents('../app/jivoo/app.json');
   return json_decode($content, true);
 }
 
+function decode_yaml_config() {
+  return spyc_load_file('./config/test-config.yaml');
+}
+
 function new_appconfig() {
-  $config = new AppConfig('../config/test-config.php');
+  $config = new AppConfig('./config/test-config.php');
   return $config;
 }
 
-include '../lib/Jivoo/bootstrap.php';
+include '../lib/Jivoo/Core/bootstrap.php';
 include '../../LAB/LabTest.php';
-Lib::import('Core');
 
-$config = new AppConfig('../config/config.php');
+include './spyc-master/spyc.php';
+
+
+$config = new AppConfig('../user/jivoo/config.php');
 $testData = $config->getArray();
 
 $rounds = 50;
 
+
 $test = new LabTest('Configuration serialization');
+$test->dump($testData);
 $json_encoded = $test->testFunction($rounds, 'json_encode', $testData);
 $test->dumpResult();
 $json_encoded = $test->testFunction($rounds, 'json_pretty_printer', $testData);
 $test->dumpResult();
-$file = fopen("../config/test-config.json", "w");
+$file = fopen("./config/test-config.json", "w");
 fwrite($file, $json_encoded);
 fclose($file);
 $php_encoded = $test->testFunction($rounds, 'php_pretty_printer', $testData);
-$file = fopen("../config/test-config.php", "w");
+$file = fopen("./config/test-config.php", "w");
 fwrite($file, "<?php\nreturn " . $php_encoded . ";\n");
 fclose($file);
 $test->dumpResult();
+$yaml_encoded = $test->testFunction($rounds, 'spyc_dump', $testData);
+$test->dumpResult();
+$file = fopen('./config/test-config.yaml', 'w');
+fwrite($file, $yaml_encoded);
+fclose($file);
 $exported = $test->testFunction($rounds, 'var_export', $testData, true);
 $test->dumpResult();
 $test->testFunction($rounds, 'json_decode', $json_encoded, true);
 $test->dumpResult();
 $test->testFunction($rounds, 'var_import', $exported);
 $test->dumpResult();
+$test->testFunction($rounds, 'new_appconfig');
 $r1 = $test->testFunction($rounds, 'include_config');
 $r2 = $test->testFunction($rounds, 'eval_config');
 $r3 = $test->testFunction($rounds, 'decode_config');
+$r4 = $test->testFunction($rounds, 'decode_yaml_config');
+$test->testFunction($rounds, 'include_config2');
+$test->testFunction($rounds, 'eval_config2');
+$test->testFunction($rounds, 'decode_config2');
 $test->dump($r1 == $r2, 'r1 == r2');
 $test->dump($r2 == $r3, 'r2 == r3');
+$test->dump($r3 == $r4, 'r3 == r4');
 
 $test->report();
 
