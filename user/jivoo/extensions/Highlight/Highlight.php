272146c52ff75f02da14dc9c1c836701cb4a7d04
Niels <nielssonnich@gmail.com>
2014-10-04 21:31:18 +0200

    Add highlight.js extension

---
+++ user/jivoo/extensions/Highlight/Highlight.php
@@ -0,0 +1,27 @@
+<?php
+// Extension
+// Name         : Highlight.js
+// Category     : JavaScript
+// Website      : https://highlightjs.org
+// Version      : 8.2
+// Dependencies : Templates Assets
+
+class Highlight extends ExtensionBase {
+  protected function init() {
+    $this->view->provide(
+      'highlight.js',
+      $this->getAsset('highlight.pack.js')
+    );
+    $this->view->provide(
+      'highlight-init.js',
+      $this->getAsset('highlight-init.js')
+    );
+    $this->view->provide(
+      'highlight-style.css',
+      $this->getAsset('styles/solarized_dark.css')
+    );
+    $this->view->script('highlight.js');
+    $this->view->script('highlight-init.js');
+    $this->view->style('highlight-style.css');
+  }
+}
