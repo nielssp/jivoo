71a92bb24dff65727080fc194d9c2ba7001dca25
Niels Sonnich Poulsen <nielssonnich@gmail.com>
2014-10-07 20:15:06 +0200

    Fix replacement of spaces in auto permalink

--- app/jivoo/assets/js/permalinks.js
+++ app/jivoo/assets/js/permalinks.js
@@ -2,7 +2,7 @@ $(function() {
   $('input[type=text][data-auto-permalink]').keyup(function() {
     var target = $(this).data('auto-permalink');
     $('#' + target).val(
-      $(this).val().toLowerCase().replace(/[^a-z0-9 ]/g, '').replace(' ', '-')
+      $(this).val().toLowerCase().replace(/[^a-z0-9 ]/g, '').replace(/ /g, '-')
     );
   });
 });
