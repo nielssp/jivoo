f47549e3a6c01c63bb521b89622ba10bed658832
Niels Sonnich Poulsen <nielssonnich@gmail.com>
2014-10-14 18:15:06 +0200

    Remove old extension code

--- lib/Jivoo/Extensions/Extensions.php
+++ lib/Jivoo/Extensions/Extensions.php
@@ -117,6 +117,8 @@ class Extensions extends LoadableModule {
   
   public function getModule($name) {
     if (!isset($this->e->$name)) {
+      if (!isset($this->loadList[$name]))
+        throw new ExtensionNotFoundException(tr('Extension not in load list: "%1"', $name));
       $this->triggerEvent('beforeLoadExtension', new LoadExtensionEvent($this, $name));
       Lib::assumeSubclassOf($name, 'ExtensionModule');
       $extension = $this->loadList[$name];
@@ -133,97 +135,12 @@ class Extensions extends LoadableModule {
     return $this->m;
   }
 
-  public function getExtension($extension) {
-    if (!isset($this->extensions[$extension])) {
-      $this->triggerEvent('beforeLoadExtension', new LoadExtensionEvent($this, $extension));
-      if (isset($this->loading[$extension])) {
-        throw new ExtensionInvalidException(
-          tr(
-            'Circular dependency detected when attempting to load the "%1" extension.',
-            $extension));
-      }
-      $this->loading[$extension] = true;
-      if (!file_exists(
-        $this->p('extensions', $extension . '/' . $extension . '.php'))) {
-        throw new ExtensionNotFoundException(
-          tr('The "%1" extension could not be found', $extension));
-      }
-      require_once(
-        $this->p('extensions', $extension . '/' . $extension . '.php')
-      );
-      if (!Lib::classExists($extension, false)) {
-        throw new ExtensionInvalidException(
-          tr('The "%1" extension does not have a main class', $extension));
-      }
-
-      $info = $this->getInfo($extension);
-      if (!$info) {
-        throw new ExtensionInvalidException(
-          tr('The "%1" extension is invalid', $extension));
-      }
-      $extensions = array();
-      foreach ($info['dependencies']['extensions'] as $dependency => $versionInfo) {
-        if ($dependency == $extension) {
-          throw new ExtensionInvalidException(
-            tr('The "%1" extension depends on itself', $extension));
-        }
-        try {
-          $extensions[$dependency] = $this->getExtension($dependency);
-        }
-        catch (ExtensionNotFoundException $ex) {
-          trigger_error(
-            tr(
-              'Extension "%1" uninstalled. Missing extension dependency: "%2".',
-              $extension, $dependency), E_USER_WARNING);
-          $this->uninstall($extension);
-          return false;
-        }
-      }
-      foreach ($info['dependencies']['modules'] as $dependency => $versionInfo) {
-        if (!$this->app->hasModule($dependency)) {
-          trigger_error(
-            tr('Extension "%1" uninstalled. Missing module dependency: "%2".',
-              $extension, $dependency), E_USER_WARNING);
-          $this->uninstall($extension);
-          return false;
-        }
-      }
-      $config = $this->config['config']->getSubset($extension);
-      $this->extensions[$extension] = new $extension($this->app, $config, $extensions);
-      $this->triggerEvent('afterLoadExtension', new LoadExtensionEvent($this, $extension, $this->extensions[$extension]));
-    }
-    return $this->extensions[$extension];
-  }
-  
-  public function getExtensions($names) {
-    $extensions = array();
-    foreach ($names as $name)
-      $extensions[$name] = $this->getExtension($name);
-    return $extensions;
+  public function hasModule($name) {
+    return isset($this->e->$name);
   }
 
-  private function updateConfig() {
-    $this->config['installed'] = implode(' ', $this->installed);
-  }
-
-  public function getInfo($extension) {
-    if (isset($this->info[$extension])) {
-      return $this->info[$extension];
-    }
-    $meta = FileMeta::read(
-      $this->p('extensions', $extension . '/' . $extension . '.php'));
-    if (!$meta OR $meta['type'] != 'extension') {
-      return false;
-    }
-    if (!isset($meta['name'])) {
-      $meta['name'] = $extension;
-    }
-    $this->info[$extension] = $meta;
-    return $meta;
-  }
-
-  public function isInstalled($extension) {
-    return in_array($extension, $this->installed);
+  public function isEnabled($extension) {
+    return in_array($extension, $this->config['import']);
   }
   
   public function enable($extension) {
@@ -232,29 +149,6 @@ class Extensions extends LoadableModule {
   
   public function disable($extension) {
     $this->config['import'] = array_diff($this->config['import']->getArray(), array($extension));
-  }
-
-  public function install($extension) {
-    if ($this->isInstalled($extension)) {
-      return;
-    }
-    if ($this->getInfo($extension) === false) {
-      return;
-    }
-    $this->installed[] = $extension;
-    $this->updateConfig();
-  }
-
-  public function uninstall($extension, $deleteConfig = false) {
-    $key = array_search($extension, $this->installed);
-    if ($key === false) {
-      return;
-    }
-    unset($this->installed[$key]);
-    $this->updateConfig();
-    if ($deleteConfig) {
-      $this->unconfigure($extension);
-    }
   }
 
   public function unconfigure($extension) {
