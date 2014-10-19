8887e6c43063394992aa0b87763568f613a3d6e4
Niels <nielssonnich@gmail.com>
2014-10-19 17:30:03 +0200

    Content extensions regex test

--- tests/test-formats.php
+++ tests/test-formats.php
@@ -1,22 +1,43 @@
 <?php
-include('../app/essentials.php');
+include('../lib/Jivoo/Core/bootstrap.php');
+
+class ContentExtensions {
+
+
+  private function replace($matches) {
+    $function = $matches[1];
+    preg_match_all('/\s+(?:([a-z]+)=)?"((?:[^\\\\"]|\\\\.)*)"/im', $matches[2], $parameters);
+    for ($i = 0; $i < count($parameters[0]); $i++) {
+      echo $parameters[1][$i] . '=';
+      var_dump($parameters[2][$i]);
+    }
+
+  }
+  
+  public function compile($content) {
+    return preg_replace_callback(
+      '/\{\{\s*([a-z]+)((?:\s+([a-z]+=)?"(?:[^\\\\"]|\\\\.)*")*)\s*\}\}/im',
+      array($this, 'replace'), $content
+    );
+  }
+}
 
 echo '<pre>';
 
-$format = new TextFormat();
+$content = 'Edit the Expression & Text to see matches. Roll over matches or
+the expression for details. Undo mistakes with ctrl-z. Save & Share expressions
+with friends or the Community. A full Reference & Help is available
+in {{link "Posts::view::15"}}, or {{break}} watch the video Tutorial.
 
-$editorContent = <<<END
-Hej verden
-hvordan går det?
+{{figure file="test.png" caption="An image
+with a \\"multiline\\" caption"}}
 
-Halløj igen! Tjek min side http://apakoh.dk
-END;
+Sample text for testing:';
 
-$dbContent = $format->toHtml($editorContent);
-echo 'In db:' . PHP_EOL;
-echo h($dbContent);
-echo PHP_EOL . PHP_EOL;
-echo 'In editor:' . PHP_EOL;
-echo h($format->fromHtml($dbContent));
+$compiler = new ContentExtensions;
+
+$compiler->compile($content);
+
+var_dump(Logger::getLog());
 
 echo '</pre>';
