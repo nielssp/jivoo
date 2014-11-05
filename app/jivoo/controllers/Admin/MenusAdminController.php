91b9216e0f54783711899543045baaac9e418e85
Niels Sonnich Poulsen <nielssonnich@gmail.com>
2014-11-05 16:45:32 +0100

    Useless menu management interface

--- app/jivoo/controllers/Admin/MenusAdminController.php
+++ app/jivoo/controllers/Admin/MenusAdminController.php
@@ -1,8 +1,11 @@
 <?php
 class MenusAdminController extends AdminController {
 
+  protected $models = array('Link');
+  
   public function index() {
     $this->title = tr('Menus');
+    $this->links = $this->Link;
     return $this->render();
   }
 }
