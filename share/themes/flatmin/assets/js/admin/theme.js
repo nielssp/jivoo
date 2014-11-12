6bb1b4d65ec92a8ed4f41ebb791300536afc47d6
Niels Sonnich Poulsen <nielssonnich@gmail.com>
2014-11-12 16:29:54 +0100

    "share"-directory for framework-bundled extensions and themes

---
+++ share/themes/flatmin/assets/js/admin/theme.js
@@ -0,0 +1,9 @@
+$(function() {
+  $('.toggle-menu').click(function() {
+    $('body').toggleClass('menu-open');
+  });
+  $('nav > ul > li > a').click(function() {
+    $('nav > ul > li > a').removeClass('current');
+    $(this).addClass('current');
+  });
+});
