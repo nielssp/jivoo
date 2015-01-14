2d82e0595a39e082a0bb3b63e8b462e93836a4b0
Niels Sonnich Poulsen <nielssonnich@gmail.com>
2015-01-14 22:32:58 +0100

    Fix paths in example index file.

--- public/index.example.php
+++ public/index.example.php
@@ -9,14 +9,14 @@
  * @since 0.1.0
  */
 
-require_once '../../lib/Jivoo/Core/bootstrap.php';
+require_once '../lib/Jivoo/Core/bootstrap.php';
 
 $app = new App(
-  '../../app/',
-  '../../user/',
+  '../app/',
+  '../user/',
   basename(__FILE__)
 );
 
-$app->paths->share = '../../share';
+$app->paths->share = '../share';
 
-$app->run('development');
+$app->run('production');
