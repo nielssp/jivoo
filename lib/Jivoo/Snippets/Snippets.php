a408b86c946517df21580b4e13379bef9c86aa73
Niels Sonnich Poulsen <nielssonnich@gmail.com>
2015-02-10 17:58:28 +0100

    New module: Snippets.

---
+++ lib/Jivoo/Snippets/Snippets.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Snippets module.
+ * @package Jivoo\Snippets
+ */
+class Console extends LoadableModule {
+  /**
+   * {@inheritdoc}
+   */
+  protected $modules = array('Routing', 'Templates');
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function init() {
+  }
+}
