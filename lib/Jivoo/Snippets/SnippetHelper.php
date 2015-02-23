420d1e79fb23f68990045cc2980158708066c97e
Niels Sonnich Poulsen <nielssonnich@gmail.com>
2015-02-23 17:03:30 +0100

    Add snippet helper.

---
+++ lib/Jivoo/Snippets/SnippetHelper.php
@@ -0,0 +1,22 @@
+<?php
+// Jivoo
+// Copyright (c) 2015 Niels Sonnich Poulsen (http://nielssp.dk)
+// Licensed under the MIT license.
+// See the LICENSE file or http://opensource.org/licenses/MIT for more information.
+namespace Jivoo\Snippets;
+
+use Jivoo\Helpers\Helper;
+
+/**
+ * Find and embed snippets.
+ */
+class SnippetHelper extends Helper {
+  
+  public function __get($snippet) {
+    
+  }
+  
+  public function __call($snippet, $parameters) {
+    
+  }
+}
