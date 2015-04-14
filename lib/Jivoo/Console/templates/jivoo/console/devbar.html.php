01be550359928ff7a819cddaa5e6321a69a368fe
Niels Sonnich Poulsen <nielssonnich@gmail.com>
2015-04-14 16:31:44 +0200

    Fix devbar positioning.

--- lib/Jivoo/Console/templates/jivoo/console/devbar.html.php
+++ lib/Jivoo/Console/templates/jivoo/console/devbar.html.php
@@ -6,6 +6,9 @@ $this->import('jivoo-console.css');
 <div id="jivoo-console">
 <div class="jivoo-devbar">
 <div class="jivoo-devbar-handle">Jivoo Devbar</div>
+<a href="#" class="jivoo-devbar-system">
+System
+</a>
 <a href="#" class="jivoo-devbar-log">
 Log
 <span class="jivoo-devbar-count jivoo-devbar-log-count">0</span>
