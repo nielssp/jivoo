f32e70dfce265339ea26b4ed82bc76b3ee2ae120
Niels <nielssonnich@gmail.com>
2015-04-19 14:20:42 +0200

    Add overflow menu to devbar.

--- lib/Jivoo/Console/templates/jivoo/console/devbar.html.php
+++ lib/Jivoo/Console/templates/jivoo/console/devbar.html.php
@@ -14,6 +14,8 @@ Log
 <span class="jivoo-devbar-count jivoo-devbar-log-count">0</span>
 </a></li>
 </ul>
+<ul class="jivoo-devbar-tools-more">
+</ul>
 <a href="#" class="jivoo-devbar-more">&raquo;</a>
 <div class="ui-resizable-handle ui-resizable-e">
 </div>
