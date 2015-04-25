d1e514ecbd6df205832b529b28850b87afa59a6f
Niels <nielssonnich@gmail.com>
2015-04-25 17:24:15 +0200

    Reimplement devbar API.

--- lib/Jivoo/Console/templates/jivoo/console/devbar.html.php
+++ lib/Jivoo/Console/templates/jivoo/console/devbar.html.php
@@ -6,13 +6,6 @@ $this->import('jivoo-console.css');
 <div id="jivoo-devbar">
 <div class="jivoo-devbar-handle">Jivoo Devbar</div>
 <ul class="jivoo-devbar-tools">
-<li><a href="#" class="jivoo-devbar-system">
-System
-</a></li>
-<li><a href="#" class="jivoo-devbar-log">
-Log
-<span class="jivoo-devbar-count jivoo-devbar-log-count">0</span>
-</a></li>
 </ul>
 <ul class="jivoo-devbar-tools-more">
 </ul>
