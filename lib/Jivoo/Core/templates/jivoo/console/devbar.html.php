2c49f3175086d45a0194886572dcb0fc08d8c1df
Niels Sonnich Poulsen <nielssonnich@gmail.com>
2015-06-18 17:27:10 +0200

    New static devbar thing.

--- lib/Jivoo/Core/templates/jivoo/console/devbar.html.php
+++ lib/Jivoo/Core/templates/jivoo/console/devbar.html.php
@@ -3,6 +3,27 @@ $this->import('jivoo-console.js');
 $this->import('jivoo-console.css');
 ?>
 
+<div id="jivoo-dev-tools" class="jivoo-dev-tools-bottom">
+  <div class="jivoo-devbar">
+    <div class="jivoo-devbar-handle">Development</div>
+    <ul class="jivoo-devbar-tools">
+      <li><a href="#">Log</a></li>
+      <li><a href="#">Data</a></li>
+      <li><a href="#">Generate</a></li>
+      <li><a href="#">I18n</a></li>
+      <li><a href="#">Release</a></li>
+    </ul>
+    <ul class="jivoo-devbar-settings">
+      <li>
+        <label class="jivoo-devbar-autohide"><input type="checkbox"> Fade</label>
+      </li>
+      <li>
+        <label class="jivoo-devbar-autohide"><input type="checkbox"> Hide</label>
+      </li>
+    </ul>
+  </div>
+</div>
+
 <div id="jivoo-devbar">
 <div class="jivoo-devbar-handle">Jivoo Devbar</div>
 <ul class="jivoo-devbar-tools">
