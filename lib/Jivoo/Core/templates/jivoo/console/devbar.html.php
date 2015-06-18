0df2bf8256f81194ce05deb2ba67ef148ae543f5
Niels Sonnich Poulsen <nielssonnich@gmail.com>
2015-06-18 17:35:54 +0200

    More of new devbar.

--- lib/Jivoo/Core/templates/jivoo/console/devbar.html.php
+++ lib/Jivoo/Core/templates/jivoo/console/devbar.html.php
@@ -7,30 +7,17 @@ $this->import('jivoo-console.css');
   <div class="jivoo-devbar">
     <div class="jivoo-devbar-handle">Development</div>
     <ul class="jivoo-devbar-tools">
-      <li><a href="#">Log</a></li>
-      <li><a href="#">Data</a></li>
-      <li><a href="#">Generate</a></li>
-      <li><a href="#">I18n</a></li>
-      <li><a href="#">Release</a></li>
     </ul>
     <ul class="jivoo-devbar-settings">
       <li>
-        <label class="jivoo-devbar-autohide"><input type="checkbox"> Fade</label>
+        <label class="jivoo-devbar-autohide"><input type="checkbox" class="jivoo-devbar-fade"> Fade</label>
       </li>
       <li>
-        <label class="jivoo-devbar-autohide"><input type="checkbox"> Hide</label>
+        <label class="jivoo-devbar-autohide"><input type="checkbox" class="jivoo-devbar-hide"> Hide</label>
       </li>
     </ul>
   </div>
-</div>
-
-<div id="jivoo-devbar">
-<div class="jivoo-devbar-handle">Jivoo Devbar</div>
-<ul class="jivoo-devbar-tools">
-</ul>
-<ul class="jivoo-devbar-tools-more">
-</ul>
-<a href="#" class="jivoo-devbar-more">&raquo;</a>
-<div class="ui-resizable-handle ui-resizable-e">
-</div>
+  <div class="jivoo-dev-frame">
+    Hello, World!
+  </div>
 </div>
