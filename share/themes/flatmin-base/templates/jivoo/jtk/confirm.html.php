0f6d2d6790514144565c498265b7e0f3a89174b7
Niels Sonnich Poulsen <nielssonnich@gmail.com>
2015-07-01 11:30:30 +0200

    Skin list theming.

--- share/themes/flatmin-base/templates/jivoo/jtk/confirm.html.php
+++
@@ -1,9 +0,0 @@
-<?php echo $Form->form(); ?>
-
-<p><?php echo $confirmation; ?></p>
-
-<?php echo $Form->submit(tr('OK')); ?>
-
-<?php echo $Form->submit(tr('Cancel'), array('name' => 'cancel')); ?>
-
-<?php echo $Form->end(); ?>
