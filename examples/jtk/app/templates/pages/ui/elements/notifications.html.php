94da4c06efb0dd6c48ab2acba0916b19e92cab0a
Niels Sonnich Poulsen <nielssonnich@gmail.com>
2015-07-02 23:44:20 +0200

    Loading screens demo.

--- examples/jtk/app/templates/pages/ui/elements/notifications.html.php
+++ examples/jtk/app/templates/pages/ui/elements/notifications.html.php
@@ -13,11 +13,30 @@
 </p>
 
 <p>
-Loading:
+Loading notification:
 <?php echo $Icon->button(tr('Start'), null, array('class' => 'load-start')); ?>
 
 <?php echo $Icon->button(tr('Stop'), null, array('class' => 'load-stop')); ?>
 </p>
+
+<p>
+Apply loading screen:
+
+<?php echo $Icon->button(tr('Apply to block'), null, array('class' => 'load-block')); ?>
+
+<?php echo $Icon->button(tr('Apply to content'), null, array('class' => 'load-content')); ?>
+
+<?php echo $Icon->button(tr('Apply to body'), null, array('class' => 'load-body')); ?>
+</p>
+
+<div class="row-1-1-1">
+<div class="cell">
+<div class="block" id="block">
+<div class="block-header">Loading screen</div>
+<div class="block-content">Lorem ipsum</div>
+</div>
+</div>
+</div>
 
 <script type="text/javascript">
 $(function() {
@@ -26,5 +45,29 @@ $(function() {
   });
   $('.load-start').click(JIVOO.notifications.startLoading);
   $('.load-stop').click(JIVOO.notifications.stopLoading);
+
+  $.fn.testLoadScreen = function() {
+    var $screen = $('<div class="loading-screen">');
+    $screen.hide();
+    $screen.appendTo(this);
+    $screen.fadeIn(100);
+    setTimeout(function() {
+      $screen.fadeOut(100, function() {
+        $screen.remove()
+      });
+    }, 2000);
+  };
+  
+  $('.load-block').click(function() {
+    $('#block').testLoadScreen();
+  });
+  
+  $('.load-content').click(function() {
+    $('#main').testLoadScreen();
+  });
+  
+  $('.load-body').click(function() {
+    $('body').testLoadScreen();
+  });
 });
 </script>
