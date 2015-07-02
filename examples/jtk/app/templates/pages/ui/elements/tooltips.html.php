49ee39a1742cbf7f68e25374f9a8f3125c523d2e
Niels Sonnich Poulsen <nielssonnich@gmail.com>
2015-07-02 20:13:05 +0200

    Tooltips demo.

---
+++ examples/jtk/app/templates/pages/ui/elements/tooltips.html.php
@@ -0,0 +1,21 @@
+<?php $this->view->data->title = tr('Notifications'); ?>
+ 
+
+<div class="row-1-1-1">
+<div class="cell">
+<p class="center">
+<a href="#" class="button" title="This is a tooltip">Hover</a>
+</p>
+</div>
+<div class="cell">
+<p class="center">
+<a href="#" class="button" title="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum consequat, ipsum">Hover</a>
+</p>
+</div>
+<div class="cell">
+<p class="center">
+<a href="#" class="button" title="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum consequat, ipsum at porttitor iaculis, nibh neque accumsan dui, sed sodales orci ligula eu mauris.">Hover</a>
+</p>
+</div>
+
+</div>
