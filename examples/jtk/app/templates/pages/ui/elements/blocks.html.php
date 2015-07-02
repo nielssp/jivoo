22058b218ef19c1ed2d7f97f26d8ac0b8f03fb10
Niels Sonnich Poulsen <nielssonnich@gmail.com>
2015-07-02 20:04:44 +0200

    Grid and blocks demo.

---
+++ examples/jtk/app/templates/pages/ui/elements/blocks.html.php
@@ -0,0 +1,126 @@
+<?php $this->view->data->title = tr('Blocks'); ?>
+
+<style type="text/css">
+
+.block {
+  background-color: #f1f1f1;
+  margin-bottom: 20px;
+}
+.block > .block-header  {
+  padding: 16px;
+  text-transform: uppercase;
+  font-weight: bold;
+  background-color: #727272;
+  color: #fff;
+}
+.block > .block-content {
+  padding: 16px;
+}
+.block > .block-content > p:last-child {
+  margin: 0;
+}
+
+.block-info > .block-header {
+  background-color: #2272cc;
+}
+
+.block-success > .block-header {
+  background-color: #22cc22;
+}
+
+.block-error > .block-header {
+  background-color: #cc2222;
+}
+
+</style>
+
+
+<div class="block">
+<div class="block-header">A block</div>
+<div class="block-content">
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sed pharetra lorem. Nunc auctor luctus tellus a faucibus. Quisque dictum in eros sed consequat. Vestibulum consequat, ipsum at porttitor iaculis, nibh neque accumsan dui, sed sodales orci ligula eu mauris.</p>
+</div>
+</div>
+
+<div class="row-1-1-1">
+
+<div class="cell">
+<div class="block">
+<div class="block-header">A block</div>
+<div class="block-content">
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sed pharetra lorem. Nunc auctor luctus tellus a faucibus. Quisque dictum in eros sed consequat.</p>
+<p>Vestibulum consequat, ipsum at porttitor iaculis, nibh neque accumsan dui, sed sodales orci ligula eu mauris.</p>
+</div>
+</div>
+</div>
+
+<div class="cell">
+<div class="block">
+<div class="block-header">A block</div>
+<div class="block-content">
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum consequat, ipsum at porttitor iaculis, nibh neque accumsan dui, sed sodales orci ligula eu mauris.</p>
+</div>
+</div>
+</div>
+
+<div class="cell">
+<div class="block">
+<div class="block-header">A block</div>
+<div class="block-content">
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sed pharetra lorem. Vestibulum consequat, ipsum at porttitor iaculis, nibh neque accumsan dui, sed sodales orci ligula eu mauris.</p>
+</div>
+</div>
+</div>
+</div>
+
+<div class="row-1-1-1">
+
+<div class="cell">
+<div class="block block-success">
+<div class="block-header">A block</div>
+<div class="block-content">
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sed pharetra lorem. Nunc auctor luctus tellus a faucibus. Quisque dictum in eros sed consequat. Vestibulum consequat, ipsum at porttitor iaculis, nibh neque accumsan dui, sed sodales orci ligula eu mauris.</p>
+</div>
+</div>
+</div>
+
+<div class="cell">
+<div class="block block-info">
+<div class="block-header">A block</div>
+<div class="block-content">
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sed pharetra lorem. Nunc auctor luctus tellus a faucibus. Quisque dictum in eros sed consequat. Vestibulum consequat, ipsum at porttitor iaculis, nibh neque accumsan dui, sed sodales orci ligula eu mauris.</p>
+</div>
+</div>
+</div>
+
+<div class="cell">
+<div class="block block-error">
+<div class="block-header">A block</div>
+<div class="block-content">
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sed pharetra lorem. Nunc auctor luctus tellus a faucibus. Quisque dictum in eros sed consequat. Vestibulum consequat, ipsum at porttitor iaculis, nibh neque accumsan dui, sed sodales orci ligula eu mauris.</p>
+</div>
+</div>
+</div>
+</div>
+
+
+<div class="row-3-1">
+<div class="cell">
+<div class="block">
+<div class="block-header">A block</div>
+<div class="block-content">
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sed pharetra lorem. Nunc auctor luctus tellus a faucibus. Quisque dictum in eros sed consequat. Vestibulum consequat, ipsum at porttitor iaculis, nibh neque accumsan dui, sed sodales orci ligula eu mauris.</p>
+</div>
+</div>
+</div>
+
+<div class="cell">
+<div class="block">
+<div class="block-header">A block</div>
+<div class="block-content">
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sed pharetra lorem. Nunc auctor luctus tellus a faucibus. Quisque dictum in eros sed consequat. Vestibulum consequat, ipsum at porttitor iaculis, nibh neque accumsan dui, sed sodales orci ligula eu mauris.</p>
+</div>
+</div>
+</div>
+
+</div>
---
+++ examples/jtk/app/templates/pages/ui/elements/grid.html.php
@@ -0,0 +1,90 @@
+<?php $this->view->data->title = tr('Grid'); ?>
+
+<h2>1:1</h2>
+
+<div class="row-1-1">
+<div class="cell">
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sed pharetra lorem. Nunc auctor luctus tellus a faucibus. Quisque dictum in eros sed consequat.</p>
+<p>Vestibulum consequat, ipsum at porttitor iaculis, nibh neque accumsan dui, sed sodales orci ligula eu mauris.</p>
+</div>
+<div class="cell">
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum consequat, ipsum at porttitor iaculis, nibh neque accumsan dui, sed sodales orci ligula eu mauris.</p>
+</div>
+</div>
+
+<h2>1:1:1</h2>
+
+<div class="row-1-1-1">
+<div class="cell">
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sed pharetra lorem. Nunc auctor luctus tellus a faucibus. Quisque dictum in eros sed consequat.</p>
+<p>Vestibulum consequat, ipsum at porttitor iaculis, nibh neque accumsan dui, sed sodales orci ligula eu mauris.</p>
+</div>
+<div class="cell">
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum consequat, ipsum at porttitor iaculis, nibh neque accumsan dui, sed sodales orci ligula eu mauris.</p>
+</div>
+<div class="cell">
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sed pharetra lorem. Vestibulum consequat, ipsum at porttitor iaculis, nibh neque accumsan dui, sed sodales orci ligula eu mauris.</p>
+</div>
+</div>
+
+
+<h2>1:1:1:1</h2>
+
+<div class="row-1-1-1-1">
+<div class="cell">
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sed pharetra lorem. Nunc auctor luctus tellus a faucibus. Quisque dictum in eros sed consequat.</p>
+<p>Vestibulum consequat, ipsum at porttitor iaculis, nibh neque accumsan dui, sed sodales orci ligula eu mauris.</p>
+</div>
+<div class="cell">
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum consequat, ipsum at porttitor iaculis, nibh neque accumsan dui, sed sodales orci ligula eu mauris.</p>
+</div>
+<div class="cell">
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sed pharetra lorem. Vestibulum consequat, ipsum at porttitor iaculis, nibh neque accumsan dui, sed sodales orci ligula eu mauris.</p>
+</div>
+<div class="cell">
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sed pharetra lorem. Vestibulum consequat, ipsum at porttitor iaculis, nibh neque accumsan dui, sed sodales orci ligula eu mauris.</p>
+<p>Quisque dictum in eros sed consequat.</p>
+</div>
+</div>
+
+
+<h2>1:2</h2>
+
+<div class="row-1-2">
+<div class="cell">
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sed pharetra lorem. Nunc auctor luctus tellus a faucibus. Quisque dictum in eros sed consequat.</p>
+<p>Vestibulum consequat, ipsum at porttitor iaculis, nibh neque accumsan dui, sed sodales orci ligula eu mauris.</p>
+</div>
+<div class="cell">
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum consequat, ipsum at porttitor iaculis, nibh neque accumsan dui, sed sodales orci ligula eu mauris.</p>
+</div>
+</div>
+
+
+<h2>1:2:1</h2>
+
+<div class="row-1-2-1">
+<div class="cell">
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sed pharetra lorem. Nunc auctor luctus tellus a faucibus. Quisque dictum in eros sed consequat.</p>
+<p>Vestibulum consequat, ipsum at porttitor iaculis, nibh neque accumsan dui, sed sodales orci ligula eu mauris.</p>
+</div>
+<div class="cell">
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum consequat, ipsum at porttitor iaculis, nibh neque accumsan dui, sed sodales orci ligula eu mauris.</p>
+</div>
+<div class="cell">
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sed pharetra lorem. Vestibulum consequat, ipsum at porttitor iaculis, nibh neque accumsan dui, sed sodales orci ligula eu mauris.</p>
+</div>
+</div>
+
+
+<h2>3:1</h2>
+
+<div class="row-3-1">
+<div class="cell">
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sed pharetra lorem. Nunc auctor luctus tellus a faucibus. Quisque dictum in eros sed consequat.</p>
+<p>Vestibulum consequat, ipsum at porttitor iaculis, nibh neque accumsan dui, sed sodales orci ligula eu mauris.</p>
+</div>
+<div class="cell">
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum consequat, ipsum at porttitor iaculis, nibh neque accumsan dui, sed sodales orci ligula eu mauris.</p>
+</div>
+</div>
