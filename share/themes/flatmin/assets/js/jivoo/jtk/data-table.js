06b1994ff81babb42a2c16f6605354fb1adeed31
Niels Sonnich Poulsen <nielssonnich@gmail.com>
2015-07-16 14:11:38 +0200

    Fix data table bulk actions and action icons.

--- share/themes/flatmin/assets/js/jivoo/jtk/data-table.js
+++ share/themes/flatmin/assets/js/jivoo/jtk/data-table.js
@@ -71,13 +71,13 @@ $(function() {
       var action = $button.data('action');
       if (allSelected) {
         var filter = $dataTable.find('#filter_filter').val();
-        var action = action.replace('?', '') + '?filter=' + filter;
+        var action = action.replace('%3F', '') + '?filter=' + filter;
       }
       else {
         var ids = $table.find('td input[type=checkbox]:checked').map(function() {
           return $(this).val();
         }).get().join();
-        action = action.replace('?', ids);
+        action = action.replace('%3F', ids);
       }
       if (method == 'get') {
         location.href = action;
