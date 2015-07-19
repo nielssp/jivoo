bc64b8dad079b775c1075391d81053e7b4dd66a7
Niels Sonnich Poulsen <nielssonnich@gmail.com>
2015-07-19 11:24:10 +0200

    Add compiled templates to minimal example application.

---
+++ examples/minimal/app/templates/compiled/not-found.html.php
@@ -0,0 +1,4 @@
+<h1>Not found</h1>
+<p>This is the
+<code>not-found.html.php</code>
+template.</p>
--- examples/minimal/app/templates/pages/example.html.php
+++
@@ -1,3 +0,0 @@
-<h1>Example page 1</h1>
-
-<p>This is the template <code>app/templates/pages/example.html.php</code>.</p>
