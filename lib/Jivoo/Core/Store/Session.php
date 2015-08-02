4f6f2167b9277ca8c65e09a2e7e6497ddbbd1f22
Niels Sonnich Poulsen <nielssonnich@gmail.com>
2015-08-02 11:40:43 +0200

    Remove unnecessary methods.

--- lib/Jivoo/Core/Store/Session.php
+++ lib/Jivoo/Core/Store/Session.php
@@ -11,23 +11,6 @@ namespace Jivoo\Core\Store;
  */
 class Session extends State {
   /**
-   * Construct sessison.
-   * @param IStore $store Store to load/save data from/to.
-   * @param bool $mutable Whether session is mutable (true) or read-only (false).
-   */
-  public function __construct(IStore $store, $mutable = true) {
-    parent::__construct();
-    $this->store = $store;
-    try {
-      $this->store->open($mutable);
-      $this->data = $this->store->read();
-    }
-    catch (StoreException $e) {
-      throw new StateInvalidException(tr('Could not read state: %1', $e->getMessage()), null, $e);
-    }
-  }
-  
-  /**
    * {@inheritdoc}
    */
   protected function update() {
@@ -37,18 +20,5 @@ class Session extends State {
       return;
     $this->store->write($this->data);
     $this->updated = false;
-  }
-  
-  /**
-   * Close, save (if mutable), and unlock state data.
-   * @throws StateClosedException If the state has already been closed.
-   */
-  public function close() {
-    if (!isset($this->store))
-      throw new StateClosedException(tr('State already closed.'));
-    if ($this->updated and $this->store->isMutable())
-      $this->store->write($this->data);
-    $this->store->close();
-    $this->store = null;
   }
 }
