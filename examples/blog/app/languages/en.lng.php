1f59bbf9c5803e6b43aee667caead5e19f987664
Niels Sonnich Poulsen <nielssonnich@gmail.com>
2015-08-06 16:43:25 +0200

    Update examples.

---
+++ examples/blog/app/languages/en.lng.php
@@ -0,0 +1,149 @@
+<?php
+
+$l = new \Jivoo\Core\Localization();
+
+$l->name = "English";
+$l->localName = "English";
+
+$l->set(
+  'My Blog',
+  'My Blog'
+);
+$l->set(
+  'Settings',
+  'Settings'
+);
+$l->set(
+  'Title',
+  'Title'
+);
+$l->set(
+  'Username',
+  'Username'
+);
+$l->set(
+  'Password',
+  'Password'
+);
+$l->set(
+  'Confirm password',
+  'Confirm password'
+);
+$l->set(
+  'Password changed.',
+  'Password changed.'
+);
+$l->set(
+  'Could not save configuration file.',
+  'Could not save configuration file.'
+);
+$l->set(
+  'The two passwords are not identical.',
+  'The two passwords are not identical.'
+);
+$l->set(
+  'Settings saved.',
+  'Settings saved.'
+);
+$l->set(
+  'Log in',
+  'Log in'
+);
+$l->set(
+  'Incorrect username and/or password.',
+  'Incorrect username and/or password.'
+);
+$l->set(
+  'Not found',
+  'Not found'
+);
+$l->set(
+  'Edit comment',
+  'Edit comment'
+);
+$l->set(
+  'Comment saved.',
+  'Comment saved.'
+);
+$l->set(
+  'Comment deleted.',
+  'Comment deleted.'
+);
+$l->set(
+  'Add post',
+  'Add post'
+);
+$l->set(
+  'Post saved.',
+  'Post saved.'
+);
+$l->set(
+  'Edit post',
+  'Edit post'
+);
+$l->set(
+  'Post deleted.',
+  'Post deleted.'
+);
+$l->set(
+  'Your name',
+  'Your name'
+);
+$l->set(
+  'Save',
+  'Save'
+);
+$l->set(
+  'Edit',
+  'Edit'
+);
+$l->set(
+  'Delete',
+  'Delete'
+);
+$l->set(
+  'Older',
+  'Older'
+);
+$l->set(
+  'Newer',
+  'Newer'
+);
+$l->set(
+  'Home',
+  'Home'
+);
+$l->set(
+  'Log out',
+  'Log out'
+);
+$l->set(
+  'Admin',
+  'Admin'
+);
+$l->set(
+  'Recent Posts',
+  'Recent Posts'
+);
+$l->set(
+  'Recent Comments',
+  'Recent Comments'
+);
+$l->set(
+  'The requested page or file does not exist. ',
+  'The requested page or file does not exist. '
+);
+$l->set(
+  'Comments',
+  'Comments'
+);
+$l->set(
+  'Leave a comment',
+  'Leave a comment'
+);
+$l->set(
+  '%1 on %2',
+  '%1 on %2'
+);
+
+return $l;
