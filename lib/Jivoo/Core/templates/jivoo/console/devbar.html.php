fda7bf84daa78634232d5307bdfd5dd631370b80
Niels Sonnich Poulsen <nielssonnich@gmail.com>
2015-08-06 14:59:53 +0200

    Improve devbar markup.

--- lib/Jivoo/Core/templates/jivoo/console/devbar.html.php
+++ lib/Jivoo/Core/templates/jivoo/console/devbar.html.php
@@ -8,16 +8,16 @@ $this->import(
 <div id="jivoo-dev-tools">
   <div class="jivoo-devbar">
     <div class="jivoo-devbar-handle"><?php echo tr('Development'); ?></div>
-    <ul class="jivoo-devbar-tools">
-    </ul>
-    <ul class="jivoo-devbar-settings">
-      <li>
+    <div class="jivoo-devbar-tools">
+    </div>
+    <div class="jivoo-devbar-settings">
+      <div>
         <label class="jivoo-devbar-autohide"><input type="checkbox" class="jivoo-devbar-fade"> <?php echo tr('Fade'); ?></label>
-      </li>
-      <li>
+      </div>
+      <div>
         <label class="jivoo-devbar-autohide"><input type="checkbox" class="jivoo-devbar-hide"> <?php echo tr('Hide'); ?></label>
-      </li>
-    </ul>
+      </div>
+    </div>
   </div>
   <div class="jivoo-dev-frame-container">
     <div class="jivoo-dev-frame">
