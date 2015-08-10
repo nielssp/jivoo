ae984ea5ee8c9efd5cdda974de312f4966c199ea
Niels Sonnich Poulsen <nielssonnich@gmail.com>
2015-08-10 20:57:38 +0200

    Control panel content.

--- src/Console/ControlPanel.php
+++ src/Console/ControlPanel.php
@@ -16,6 +16,73 @@ class ControlPanel extends ConsoleSnippet {
    */
   public function before() {
     $this->view->data->title = tr('Control panel');
+    $this->view->data->schemas = $this->findSchemas();
+    $this->view->data->models = $this->findModels();
+    $this->view->data->controllers = $this->findControllers();
+    $this->view->data->snippets = $this->findSnippets();
     return parent::before();
+  }
+  
+  private function findSchemas() {
+    $dir = new \DirectoryIterator($this->p('app/Schemas'));
+    $schemas = array();
+    foreach ($dir as $file) {
+      if (!$file->isDot()) {
+        $class = $this->app->n('Schemas\\' . preg_replace('/\.php$/', '', $file->getFilename()));
+        $object = new $class();
+        $schemas[] = $object;
+      }
+    }
+    return $schemas;
+  }
+  
+  private function findModels() {
+    $dir = new \DirectoryIterator($this->p('app/Models'));
+    $models = array();
+    foreach ($dir as $file) {
+      if (!$file->isDot()) {
+        $model = preg_replace('/\.php$/', '', $file->getFilename());
+        $models[] = $model;
+      }
+    }
+    return $models;
+  }
+  
+  private function findControllers(\RecursiveDirectoryIterator $dir = null, $prefix = '') {
+    if (!isset($dir))
+      $dir = new \RecursiveDirectoryIterator($this->p('app/Controllers'));
+    $controllers = array();
+    foreach ($dir as $file) {
+      if ($dir->hasChildren()) {
+        $controllers = array_merge(
+          $controllers,
+          $this->findControllers($dir->getChildren(), $file->getFilename() . '\\')
+        );
+      }
+      else if($file->isFile()){
+        $controller = preg_replace('/Controller\.php$/', '', $file->getFilename());
+        $controllers[] = $prefix . $controller;
+      }
+    }
+    return $controllers;
+  }
+  
+  private function findSnippets(\RecursiveDirectoryIterator $dir = null, $prefix = '') {
+    if (!isset($dir))
+      $dir = new \RecursiveDirectoryIterator($this->p('app/Snippets'));
+    $snippets = array();
+    foreach ($dir as $file) {
+      if ($dir->hasChildren()) {
+        $snippets = array_merge(
+          $snippets,
+          $this->findControllers($dir->getChildren(), $file->getFilename() . '\\')
+        );
+      }
+      else if($file->isFile()){
+        $snippet = preg_replace('/\.php$/', '', $file->getFilename());
+        $snippets[] = $prefix . $snippet;
+      }
+    }
+    return $snippets;
   }
 }
--- src/Core/templates/jivoo/console/control-panel.html.php
+++ src/Core/templates/jivoo/console/control-panel.html.php
@@ -1,16 +1,155 @@
+<div class="block-container">
+
+<h2><?php echo tr('Data and models'); ?></h2>
 <div class="grid-md grid-1-1">
 <div class="cell">
 
-
-
-
+<div class="block">
+<div class="block-header">
+<h3><?php echo tr('Schemas'); ?></h3>
+</div>
+<div class="block-content">
+<table>
+<thead>
+<tr>
+<th>
+<?php echo tr('Schema'); ?>
+</th>
+<th>
+<?php echo tr('Actions'); ?>
+</th>
+</tr>
+</thead>
+<tbody>
+<?php foreach ($schemas as $schema): ?>
+<tr>
+<td><?php echo h($schema->getName()); ?></td>
+<td>
+<div class="button-group">
+<?php echo $Html->link('Alter', null, 'class="button button-xs"'); ?>
+<?php echo $Html->link('Drop', null, 'class="button button-xs"'); ?>
+</div>
+</td>
+</tr>
+<?php endforeach; ?>
+</tbody>
+</table>
+</div>
+</div>
 
 </div>
 <div class="cell">
 
-
-
-
+<div class="block">
+<div class="block-header">
+<h3><?php echo tr('Models'); ?></h3>
+</div>
+<div class="block-content">
+<table>
+<thead>
+<tr>
+<th>
+<?php echo tr('Model'); ?>
+</th>
+<th>
+<?php echo tr('Actions'); ?>
+</th>
+</tr>
+</thead>
+<tbody>
+<?php foreach ($models as $model): ?>
+<tr>
+<td><?php echo h($model); ?></td>
+<td>
+<div class="button-group">
+<?php echo $Html->link('Associations', null, 'class="button button-xs"'); ?>
+<?php echo $Html->link('Delete', null, 'class="button button-xs"'); ?>
+</div>
+</td>
+</tr>
+<?php endforeach; ?>
+</tbody>
+</table>
+</div>
+</div>
 
 </div>
+</div>
+
+
+<h2><?php echo tr('Application logic'); ?></h2>
+<div class="grid-md grid-1-1">
+<div class="cell">
+
+<div class="block">
+<div class="block-header">
+<h3><?php echo tr('Controllers'); ?></h3>
+</div>
+<div class="block-content">
+<table>
+<thead>
+<tr>
+<th>
+<?php echo tr('Controller'); ?>
+</th>
+<th>
+<?php echo tr('Actions'); ?>
+</th>
+</tr>
+</thead>
+<tbody>
+<?php foreach ($controllers as $controller): ?>
+<tr>
+<td><?php echo h($controller); ?></td>
+<td>
+<div class="button-group">
+<?php echo $Html->link('Actions', null, 'class="button button-xs"'); ?>
+<?php echo $Html->link('Delete', null, 'class="button button-xs"'); ?>
+</div>
+</td>
+</tr>
+<?php endforeach; ?>
+</tbody>
+</table>
+</div>
+</div>
+
+</div>
+<div class="cell">
+
+<div class="block">
+<div class="block-header">
+<h3><?php echo tr('Snippets'); ?></h3>
+</div>
+<div class="block-content">
+<table>
+<thead>
+<tr>
+<th>
+<?php echo tr('Snippet'); ?>
+</th>
+<th>
+<?php echo tr('Actions'); ?>
+</th>
+</tr>
+</thead>
+<tbody>
+<?php foreach ($snippets as $snippet): ?>
+<tr>
+<td><?php echo h($snippet); ?></td>
+<td>
+<div class="button-group">
+<?php echo $Html->link('Delete', null, 'class="button button-xs"'); ?>
+</div>
+</td>
+</tr>
+<?php endforeach; ?>
+</tbody>
+</table>
+</div>
+</div>
+
+</div>
+</div>
+
 </div>
