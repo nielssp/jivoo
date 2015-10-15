54c3265c9bb571cffe7cfeaefaf1d04ecff46a70
Niels Sonnich Poulsen <nielssonnich@gmail.com>
2015-10-15 17:48:34 +0200

    Add filtering support to content mixin.

--- src/Content/ContentMixin.php
+++ src/Content/ContentMixin.php
@@ -8,6 +8,7 @@ namespace Jivoo\Content;
 use Jivoo\ActiveModels\ActiveModelMixin;
 use Jivoo\ActiveModels\ActiveModelEvent;
 use Jivoo\ActiveModels\ActiveRecord;
+use Jivoo\Core\Assume;
 
 /**
  * Mixin for automatically compiling content fields.
@@ -31,15 +32,24 @@ class ContentMixin extends ActiveModelMixin {
   /**
    * {@inheritdoc}
    */
-  protected $methods = array('recordDisplay', 'recordEditor');
+  protected $methods = array(
+    'recordDisplay', 'recordEditor', 'addFilter', 'getPurifierConfig'
+  );
+  
+  private $purifierConfigs = array();
+  
+  private $filters = array();
 
   /**
    * {@inheritdoc}
    */
   public function init() {
     $helper = $this->helper('Content');
-    foreach ($this->options['fields'] as $field)
+    foreach ($this->options['fields'] as $field) {
       $helper->register($this->model, $field);
+      $this->purifierConfigs[$field] = \HTMLPurifier_Config::createDefault();
+      $this->filters[$field] = array();
+    }
   }
   
   /**
@@ -47,7 +57,10 @@ class ContentMixin extends ActiveModelMixin {
    * @return string HTML.
    */
   public function recordDisplay(ActiveRecord $record, $field = 'content') {
-    return $this->helper('Content')->purify($record, $field);
+    $purifier = new \HTMLPurifier($this->getPurifierConfig($field));
+    $htmlField = $field . 'Html';
+    $html = $this->applyFilters($field, 'prerender', $record->$htmlField);
+    return $purifier->purify($html);
   }
   
   /**
@@ -58,6 +71,43 @@ class ContentMixin extends ActiveModelMixin {
     $editor = $this->helper('Content')->getEditor($record->getModel(), $field);
     return $editor($record, $field);
   }
+  
+  /**
+   * Add a filter to a field.
+   * @param string $field Field name.
+   * @param string $stage Content stage: 'preprocess', 'postprocess', 'prerender'.
+   * @param callable $callable Filter function, accepts a string and returns a
+   * string.
+   */
+  public function addFilter($field, $stage, $callable) {
+    Assume::hasKey($this->filters, $field);
+    if (!isset($this->filters[$field][$stage]))
+      $this->filters[$field][$stage] = array();
+    $this->filters[$field][$stage][] = $callable;
+  }
+  
+  /**
+   * Apply a field's filters to a string.
+   * @param string $field Field name.
+   * @param string $stage Content stage: 'preprocess', 'postprocess', 'prerender'.
+   * @param string $content Content.
+   * @return Filtered content.
+   */
+  public function applyFilters($field, $stage, $content) {
+    foreach ($this->filters[$field][$stage] as $callable)
+      $content = call_user_func_array($callable, $content);
+    return $content;
+  }
+  
+  /**
+   * Get HTMLPurifier config for field.
+   * @param string $field Field name.
+   * @return \HTMLPurifier_Config Purifier configuration object.
+   */
+  public function getPurifierConfig($field = 'content') {
+    Assume::hasKey($this->purifierConfigs, $field);
+    return $this->purifierConfigs[$field];
+  }
 
   /**
    * {@inheritdoc}
@@ -66,7 +116,7 @@ class ContentMixin extends ActiveModelMixin {
     if (!$this->options['html'] or !$this->options['text'])
       return;
     foreach ($this->options['fields'] as $field) {
-      $content = $event->record->$field;
+      $content = $this->applyFilters($field, 'preprocess', $event->record->$field);
       $formatField = $field . 'Format';
       $format = $this->helper('Content')->getFormat($event->record->$formatField);
       if (isset($format)) {
@@ -76,6 +126,7 @@ class ContentMixin extends ActiveModelMixin {
         $html = $content;
       }
       $html = $this->helper('Content')->extensions->compile($html);
+      $html = $this->applyFilters($field, 'postprocess', $html);
       if ($this->options['html']) {
         $htmlField =  $field . 'Html';
         $event->record->$htmlField = $html; 
