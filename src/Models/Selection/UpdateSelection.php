33b7b1b2d09ec43f97698481e95221d9721e9a37
Niels Sonnich Poulsen <nielssonnich@gmail.com>
2015-10-30 18:11:31 +0100

    Fix incorrect UpdateSelection interface.

--- src/Models/Selection/UpdateSelection.php
+++ src/Models/Selection/UpdateSelection.php
@@ -10,7 +10,19 @@ namespace Jivoo\Models\Selection;
  */
 interface UpdateSelection extends BasicSelection {
   /**
-   * @return array Associative array of field names and values
+   * Assign value to field. If $field is an associative array, then multiple
+   * fields are assigned. If $field contains an equals sign ('=') then $field
+   * is used as the set expression.
+   * @param string|array $field Field name or associative array of field names
+   * and values
+   * @param string $value Value
+   * @return UpdateSelection An update selection.
    */
-  public function getData();
+  public function set($field, $value = null);
+
+  /**
+   * Execute updates.
+   * @return int Number of updated records.
+   */
+  public function update();
 }
