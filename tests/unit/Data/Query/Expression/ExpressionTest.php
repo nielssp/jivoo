616845dc3d9745e3a3d562a3b6f4fb7596549c8e
Niels Sonnich Poulsen <nielssonnich@gmail.com>
2015-11-10 17:13:29 +0100

    Data expressions.

---
+++ tests/unit/Data/Query/Expression/ExpressionTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Jivoo\Data\Query\Expression;
+
+abstract class ExpressionTest extends \Jivoo\Test {
+  protected function _before() {}
+
+  protected function _after() {}
+  
+  /**
+   * @return Expression An empty expression.
+   */
+  abstract protected function getExpression();
+  
+  public function testImplements() {
+    $this->assertInstanceOf('Jivoo\Data\Query\Expression', $this->getExpression());
+  }
+  
+  protected function testEmptyExpression() {
+    $expression = $this->getExpression();
+    $this->assertEmpty($expression->getString());
+    $this->assertEmpty($expression->getVars());
+  }
+  
+  // TODO: rest
+}
