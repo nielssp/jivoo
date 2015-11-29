861a533d8d66bf4170422925bd59c063f4c7cf6c
Niels Sonnich Poulsen <nielssonnich@gmail.com>
2015-11-29 13:16:38 +0100

    Add abstract JSON package reader.

---
+++ src/Vendor/JsonPackageReader.php
@@ -0,0 +1,46 @@
+<?php
+// Jivoo
+// Copyright (c) 2015 Niels Sonnich Poulsen (http://nielssp.dk)
+// Licensed under the MIT license.
+// See the LICENSE file or http://opensource.org/licenses/MIT for more information.
+namespace Jivoo\Vendor;
+
+use Jivoo\Core\Json;
+use Jivoo\Core\JsonException;
+
+/**
+ * Reads JSON manifests.
+ */
+abstract class JsonPackageReader implements PackageReader {
+  /**
+   * Get name of manifest file.
+   * @return string 
+   */
+  public abstract function getFileName();
+  
+  /**
+   * Get package.
+   * @param array $manifest Manfiest object.
+   * @param string $path Package path.
+   * @return Package
+   */
+  public abstract function getPackage(array $manifest, $path);
+
+  /**
+   * {@inheritdoc}
+   */
+  public function read($name, $path) {
+    $file = $path . '/' . $this->getFileName();
+    if (!file_exists($file))
+      return null;
+    try {
+      $manifest = Json::decodeFile($file);
+    }
+    catch (JsonException $e) {
+      return null;
+    }
+    if (!isset($manifest['name']))
+      $manifest['name'] = $name;
+    return $this->getPackage($manifest);
+  }
+}
