db5ecafefba049a5673a5e88681808173c7417e9
Niels Sonnich Poulsen <nielssonnich@gmail.com>
2015-12-13 19:24:26 +0100

    Remove echo.

--- src/Vendor/JsonPackageReader.php
+++ src/Vendor/JsonPackageReader.php
@@ -52,7 +52,6 @@ abstract class JsonPackageReader implements PackageReader {
    */
   public function read($name, $path) {
     $file = $path . '/' . $this->getFileName();
-    echo $file . PHP_EOL;
     if (!file_exists($file))
       return null;
     try {
